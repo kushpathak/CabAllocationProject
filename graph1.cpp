3dc958fc8e597736d7e0a5517fe2cc748120afce
kush <kushagrapathak0987@gmail.com>
2021-07-11 10:18:40 +0530

    updating graph1.cpp

--- graph1.cpp
+++ graph1.cpp
@@ -1,21 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int maxsize = 1e2+5;
+
+/*
+    Class which provides functionalities for route calculation, 
+    fare calculation and addition of new routes
+*/
 class Graph{
+    //Creating an adjacency list
     map<string,vector<pair<string,int> > >adjlist;
+    //Creating a distance map i,e dist[a,b] = w
     map<pair<string,string>,int >actual;
     int fpc;
 public:
     Graph(){
         fpc = 6;
     }
-    void AddEdge(string u,string v,int w){
+    /*
+        This function basically adds an edge between two nodes in a graph.
+    */
+    void add_edge(string u,string v,int w){
        adjlist[u].push_back({v,w});
        adjlist[v].push_back({u,w});
        actual[{u,v}]=w;
        actual[{v,u}]=w;
     }
-    void Updateweight(string u,string v,int w){
+    /*
+        This function updates the weight between two nodes in the graph to a
+        new value
+    */
+    void update_weight(string u,string v,int w){
       for(int i=0;i<adjlist[u].size();i++){
 
             if(adjlist[u][i].first==v)
@@ -25,20 +39,23 @@ public:
             }
       }
     }
-    void Dijkstra(string src,string dest){
+    /*
+        This function whill calculate route between 2 nodes in a graph using
+        dijkstra algorithm.
+    */
+    void dijkstra(string src,string dest){
         map<string,int>dist;
         map<string,string>parent;
         set<pair<int,string> >st;
         for(auto i : adjlist){
             dist[i.first]=INT_MAX;
-           // parent[i.first]="1";
         }
         dist[src]=0;
         st.insert({0,src});
         int d=0;
         while(!st.empty()){
             pair<int,string>p = *st.begin();
-            //cout << p.second << endl;
+           
             st.erase(p);
             string s = p.second;
             int nodedist = p.first;
@@ -72,9 +89,14 @@ public:
             cur++;
         }
         cout << endl << endl;
-        Calculate_Fare(d);
+        calculate_fare(d);
     }
-    void Calculate_Fare(int total_dist){
+    /*  
+        This function will calculate fare based on the distance travelled 
+        The fare might also increase by a random amount depending on the
+        traffic faced 
+    */
+    void calculate_fare(int total_dist){
         srand(time(NULL));
         int fare = total_dist*fpc;
         fare += rand()%50;
@@ -91,32 +113,34 @@ public:
         cout << "\n                       Press enter to continue....                       \n";
         }while(cin.get()!='\n');
     }
-    //("pause");
 };
 Graph G;
+/*  
+    This function will be used to create edges in a graph
+*/
 void start(){
-     G.AddEdge("Noida","Ghaziabad",10);
-    G.AddEdge("Noida","Indirapuram",5);
-    G.AddEdge("Indirapuram","Ghaziabad",4);
-    G.AddEdge("Noida","Sector 62",8);
-    G.AddEdge("Sector 62","Sector 18",4);
-    G.AddEdge("Sector 18","Sector 59",3);
-    G.AddEdge("Sector 59","Sector 19",2);
-    G.AddEdge("Sector 19","Sector 14",1);
-    G.AddEdge("Sector 14","Sector 10",8);
-    G.AddEdge("Sector 62","Delhi Junction",20);
-    G.AddEdge("Delhi Junction","Chandni Chowk",5);
-    G.AddEdge("Chandni Chowk","New Delhi",10);
-    G.AddEdge("New Delhi","Chandni Chowk",7);
-    G.AddEdge("Chandni Chowk","Sadar Bazaar",19);
-    G.AddEdge("Sadar Bazaar","Canaught Place",14);
-    G.AddEdge("Canaught Place","Daulat Pura",19);
-    G.AddEdge("Daulat Pura","Lal Quila",20);
-    G.AddEdge("Lal Quila","Delhi Junction",2);
+    G.add_edge("Noida","Ghaziabad",10);
+    G.add_edge("Noida","Indirapuram",5);
+    G.add_edge("Indirapuram","Ghaziabad",4);
+    G.add_edge("Noida","Sector 62",8);
+    G.add_edge("Sector 62","Sector 18",4);
+    G.add_edge("Sector 18","Sector 59",3);
+    G.add_edge("Sector 59","Sector 19",2);
+    G.add_edge("Sector 19","Sector 14",1);
+    G.add_edge("Sector 14","Sector 10",8);
+    G.add_edge("Sector 62","Delhi Junction",20);
+    G.add_edge("Delhi Junction","Chandni Chowk",5);
+    G.add_edge("Chandni Chowk","New Delhi",10);
+    G.add_edge("New Delhi","Chandni Chowk",7);
+    G.add_edge("Chandni Chowk","Sadar Bazaar",19);
+    G.add_edge("Sadar Bazaar","Canaught Place",14);
+    G.add_edge("Canaught Place","Daulat Pura",19);
+    G.add_edge("Daulat Pura","Lal Quila",20);
+    G.add_edge("Lal Quila","Delhi Junction",2);
 }
 int main(){
 
     start();
-    G.Dijkstra("Noida","Delhi Junction");
+    G.dijkstra("Noida","Delhi Junction");
 
 }
